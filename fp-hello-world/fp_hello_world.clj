90bf0c932c6f30aa4ce5b8d715f2972b050e3441
Peter Hellberg <peter@c7.se>
2014-01-04 15:54:52 +0100

    Solution for: fp-hello-world
    Language: Clojure

---
+++ fp-hello-world/fp_hello_world.clj
@@ -0,0 +1,9 @@
+;; Namespace
+(ns fp-hello-world)
+
+;; Main function, not needed on HackerRank
+(defn -main [& args]
+
+  ;; Solution
+  (print "Hello World")
+)
---
+++ fp-hello-world/project.clj
@@ -0,0 +1,4 @@
+(defproject fp-hello-world "0.0.1"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :source-paths ["."]
+)
